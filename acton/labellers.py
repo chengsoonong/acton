6c610aadd5e4d981c276f355e580c87bcb6085c5
MatthewJA <matthew.alger@gmail.com>
2016-11-15 13:36:28 +1100

    Added file-based labeller. Resolves #18.

--- acton/labellers.py
+++ acton/labellers.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 
+import astropy.io.ascii
 import numpy
 
 
@@ -24,5 +25,53 @@ class Labeller(ABC):
         Returns
         -------
         numpy.ndarray
-            T x N x F label array.
+            T x F label array.
         """
+
+
+class ASCIITableLabeller(Labeller):
+    """Labeller that obtains labels from an ASCII table.
+
+    Attributes
+    ----------
+    path : str
+        Path to table.
+    id_col : str
+        Name of the column where IDs are stored.
+    label_col : str
+        Name of the column where binary labels are stored.
+    _table : astropy.table.Table
+        Table object.
+    """
+
+    def __init__(self, path: str, id_col: str, label_col: str):
+        """
+        path
+            Path to table.
+        id_col
+            Name of the column where IDs are stored.
+        label_col
+            Name of the column where binary labels are stored.
+        """
+        self.path = path
+        self.id_col = id_col
+        self.label_col = label_col
+        self._table = astropy.io.ascii.read(self.path)
+
+    def query(self, id_: bytes) -> numpy.ndarray:
+        """Queries the labeller.
+
+        Parameters
+        ----------
+        id_
+            ID of instance to label.
+
+        Returns
+        -------
+        numpy.ndarray
+            1 x 1 label array.
+        """
+        for row in self._table:
+            if row[self.id_col].encode('ascii') == id_:
+                return row[self.label_col].reshape((1, 1))
+        raise KeyError('Unknown id: {}'.format(self.id_))

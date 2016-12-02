52c9884ad51b33dbd51337045e529c5ebacddc63
MatthewJA <matthew.alger@gmail.com>
2016-12-02 18:24:38 +1100

    Added PredictorOutput wrapper

--- acton/proto/wrappers.py
+++ acton/proto/wrappers.py
@@ -1,6 +1,6 @@
 """Classes that wrap protobufs."""
 
-from typing import Union, List
+from typing import Union, List, Iterable
 
 import acton.database
 import acton.proto.predictors_pb2 as predictors_pb
@@ -32,6 +32,7 @@ class PredictorInput(object):
             else:
                 raise TypeError('proto should be str or protobuf.')
         self._validate_proto()
+        self._set_default()
 
     @property
     def DB(self) -> acton.database.Database:
@@ -140,3 +141,135 @@ class PredictorInput(object):
     def _set_default(self):
         """Adds default parameters to the protobuf."""
         self.proto.dtype = self.proto.dtype or 'float32'
+
+
+class PredictorOutput(object):
+    """Wrapper for the predictor output protobuf.
+
+    Attributes
+    ----------
+    proto : predictors_pb.Predictions
+        Protobuf representing the output.
+    """
+
+    def __init__(self, proto: Union[str, predictors_pb.Predictions]):
+        """
+        Parameters
+        ----------
+        proto
+            Path to .proto file, or raw protobuf itself.
+        """
+        try:
+            self.proto = acton.proto.io.read_proto(
+                proto, predictors_pb.Predictions)
+        except TypeError:
+            if isinstance(proto, predictors_pb.Predictions):
+                self.proto = proto
+            else:
+                raise TypeError('proto should be str or protobuf.')
+        self._validate_proto()
+        self._set_default()
+
+    @property
+    def DB(self) -> acton.database.Database:
+        """Gets a database context manager for the specified database.
+
+        Returns
+        -------
+        type
+            Database context manager.
+        """
+        if hasattr(self, '_DB'):
+            return self._DB
+
+        self._DB = lambda: acton.database.DATABASES[self.proto.db_class](
+            self.proto.db_path, label_dtype=self.proto.dtype)
+
+        return self._DB
+
+    @property
+    def ids(self) -> List[bytes]:
+        """Gets a list of IDs.
+
+        Returns
+        -------
+        List[bytes]
+            List of known IDs.
+        """
+        if hasattr(self, '_ids'):
+            return self._ids
+
+        self._ids = [prediction.id for prediction in self.proto.prediction]
+        return self._ids
+
+    @property
+    def predictions(self) -> numpy.ndarray:
+        """Gets predictions array specified in input.
+
+        Notes
+        -----
+        The returned array is cached by this object so future calls will not
+        need to recompile the array.
+
+        Returns
+        -------
+        numpy.ndarray
+            T x N x D NumPy array of predictions.
+        """
+        if hasattr(self, '_predictions'):
+            return self._predictions
+
+        self._predictions = []
+        for prediction in self.proto.prediction:
+            data = prediction.label
+            shape = (self.proto.n_predictors,
+                     self.proto.n_prediction_dimensions)
+            dtype = self.proto.dtype
+            self._predictions.append(
+                acton.proto.io.get_ndarray(data, shape, dtype))
+        self._predictions = numpy.array(self._predictions).transpose((1, 0, 2))
+        return self._predictions
+
+    def _validate_proto(self):
+        """Checks that the protobuf is valid and enforces constraints.
+
+        Raises
+        ------
+        ValueError
+        """
+        if self.proto.db_class not in acton.database.DATABASES:
+            raise ValueError('Invalid database class: {}'.format(
+                self.proto.db_class))
+
+        if self.proto.n_predictors < 1:
+            raise ValueError('Number of predictors must be > 0.')
+
+        if self.proto.n_prediction_dimensions < 1:
+            raise ValueError('Prediction dimension must be > 0.')
+
+        if not self.proto.db_path:
+            raise ValueError('Must specify db_path.')
+
+    def _set_default(self):
+        """Adds default parameters to the protobuf."""
+        self.proto.dtype = self.proto.dtype or 'float32'
+
+
+def from_predictions(
+        ids: Iterable[bytes],
+        predictions: numpy.ndarray) -> PredictorOutput:
+    """Converts NumPy predictions to a PredictorOutput.
+
+    Parameters
+    ----------
+    ids
+        Iterable of instance IDs.
+    predictions
+        T x N x D array of corresponding predictions.
+
+    Returns
+    -------
+    PredictorOutput
+    """
+    raise NotImplementedError()
+

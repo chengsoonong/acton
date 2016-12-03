238dc786d2a17f5cef5073103395341cf718a6bf
MatthewJA <matthew.alger@gmail.com>
2016-12-03 14:45:36 +1100

    Added delimited protobuf serialisation

--- acton/proto/io.py
+++ acton/proto/io.py
@@ -1,5 +1,7 @@
 """Functions for reading/writing to protobufs."""
 
+import struct
+
 from google.protobuf.reflection import GeneratedProtocolMessageType
 import numpy
 
@@ -27,6 +29,64 @@ def read_proto(
         proto.ParseFromString(proto_file.read())
 
     return proto
+
+
+def write_protos(path: str):
+    """Serialises many protobufs to a file.
+    
+    Parameters
+    ----------
+    path
+        Path to binary file. Will be overwritten.
+
+    Notes
+    -----
+    Coroutine. Accepts protobufs, or None to terminate and close file.
+    """
+    with open(path, 'wb') as proto_file:
+        proto = yield
+        while proto:
+            proto = proto.SerializeToString()
+            # Protobufs are not self-delimiting, so we need to store the length
+            # of each protobuf that we write. We will do this with an unsigned
+            # long long (Q).
+            length = struct.pack('<Q', len(proto))
+            proto_file.write(length)
+            proto_file.write(proto)
+            proto = yield
+    while True:
+        proto = yield
+        if proto:
+            raise RuntimeError('Cannot write protobuf to closed file.')
+
+
+def read_protos(
+        path: str,
+        Proto: GeneratedProtocolMessageType
+) -> 'GeneratedProtocolMessageType()':
+    """Reads many protobufs from a file.
+
+    Parameters
+    ----------
+    path
+        Path to binary file.
+    Proto:
+        Protocol message class (from the generated protobuf module).
+
+    Yields
+    -------
+    GeneratedProtocolMessageType
+        A parsed protobuf.
+    """
+    # This is essentially the inverse of the write_protos function.
+    with open(path, 'rb') as proto_file:
+        length = proto_file.read(8)  # long long
+        while length:
+            length, = struct.unpack('<Q', length)
+            proto = Proto()
+            proto.ParseFromString(proto_file.read(length))
+            yield proto
+            length = proto_file.read(8)
 
 
 def get_ndarray(data: list, shape: tuple, dtype: str) -> numpy.ndarray:

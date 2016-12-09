86c6d9fcf7d7a6223c8ff1314cfd2e22af8cf383
MatthewJA <matthew.alger@gmail.com>
2016-12-09 12:16:40 +1100

    Added KDEClassifier.predict_proba.

--- acton/kde_predictor.py
+++ acton/kde_predictor.py
@@ -55,11 +55,55 @@ class KDEClassifier(sklearn.base.BaseEstimator, sklearn.base.ClassifierMixin):
         sklearn.utils.validation.check_is_fitted(self, ['kdes_', 'classes_'])
         X = sklearn.utils.validation.check_array(X)
 
-        scores = numpy.zeros((len(self.classes_), X.shape[0]))
-        for label, kde in enumerate(self.kdes_):
-            scores[label, :] = kde.score_samples(X)
+        scores = self.predict_proba(X)
 
-        most_probable_indices = scores.argmax(axis=0)
+        most_probable_indices = scores.argmax(axis=1)
         assert most_probable_indices.shape[0] == X.shape[0]
 
         return numpy.array([self.classes_[i] for i in most_probable_indices])
+
+    @staticmethod
+    def _softmax(data, axis=0):
+        """Computes the softmax of an array along an axis.
+
+        Notes
+        -----
+        Adapted from https://gist.github.com/stober/1946926.
+
+        Parameters
+        ----------
+        data : array_like
+            Array of numbers.
+        axis : int
+            Axis to softmax along.
+        """
+        e_x = numpy.exp(
+            data - numpy.expand_dims(numpy.max(data, axis=axis), axis))
+        out = e_x / numpy.expand_dims(e_x.sum(axis=axis), axis)
+        return out
+
+    def predict_proba(self, X):
+        """Predicts class probabilities.
+
+        Class probabilities are normalised log densities of the kernel density
+        estimates.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+        """
+        sklearn.utils.validation.check_is_fitted(self, ['kdes_', 'classes_'])
+        X = sklearn.utils.validation.check_array(X)
+
+        scores = numpy.zeros((X.shape[0], len(self.classes_)))
+        for label, kde in enumerate(self.kdes_):
+            scores[:, label] = kde.score_samples(X)
+
+        scores = self._softmax(scores, axis=1)
+
+        assert scores.shape == (X.shape[0], len(self.classes_))
+        assert numpy.allclose(scores.sum(axis=1), numpy.ones((X.shape[0],)))
+
+        return scores
--- tests/test_kde_predictor.py
+++ tests/test_kde_predictor.py
@@ -3,12 +3,23 @@
 import unittest
 
 import acton.kde_predictor
+import numpy
 import sklearn.utils.estimator_checks
 
 
-class TestSklearnInterface(unittest.TestCase):
+class TestKDEClassifier(unittest.TestCase):
 
     def test_sklearn_interface(self):
         """KDEClassifier implements the scikit-learn interface."""
         sklearn.utils.estimator_checks.check_estimator(
             acton.kde_predictor.KDEClassifier)
+
+    def test_softmax(self):
+        """_softmax correctly evaluates a softmax on array input."""
+        for axis in range(2):
+            for _ in range(100):
+                array = numpy.random.random(size=(100, 100)) * 1000 - 500
+                softmax = acton.kde_predictor.KDEClassifier._softmax(
+                    array, axis=axis)
+                for i in softmax.sum(axis=axis):
+                    self.assertAlmostEqual(i, 1)

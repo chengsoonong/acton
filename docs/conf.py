d532c19923120563befd9675ff3c1b4c05701a62
MatthewJA <matthew.alger@gmail.com>
2017-02-15 18:17:03 +1100

    Bump version: 0.3.1 → 0.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ author = 'Matthew Alger & Cheng Soon Ong'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.1'
+version = '0.3.2'
 # The full version, including alpha/beta/rc tags.
 release = '0.3.1'
 
